8cd40a127c4806e2f017be1d86de5e7adfa7a27a
EMMANUEL <waruterewachira7@gmail.com>
2024-05-21 01:15:12 +0300

    complete

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Keypad from '../Keypad';
+import EyesOnMe from '../EyesOnMe';
+
+function App() {
+    return (
+        <div>
+            <Keypad />
+            <EyesOnMe />
+        </div>
+    );
+}
+export default App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+      const handleChange = (event) => {
+        console.log('Entering password...', event.target.value); 
+    };
     return (
-        <div></div>
-    )
+        <div>
+            <input type = "password" onChange={handleChange} />
+        </div>
+    );
 }
 
 export default Keypad;
